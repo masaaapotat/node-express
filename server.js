717cd1fd41dea49fb5e146fb112eeac1d33953ba
masaapotat <masaakavulanya@gmail.com>
2024-08-12 06:43:54 +0300

    express P2

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+// set up the middleware function
+app.use(express.static(__dirname + '/public'));
 // set up the server w use method, which takes up a callback function, which is a middleware function rhat takes up 3 paranmeters, req, res and next
 app.use((req, res) => {
-    console.log(req.headers)
+    // morgan handles the logging requsts
+    // console.log(req.headers)
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server</h1></body></html>');
